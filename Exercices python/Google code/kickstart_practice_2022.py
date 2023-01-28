b69f1bd5e7d96f3dcef70d03d50e6ee35d60b549
Adrien <magiczvirtuoz@gmail.com>
2023-01-28 17:28:56 +0100

    Delete Exercices python directory

--- Exercices python/Google code/kickstart_practice_2022.py
+++
@@ -1,7 +0,0 @@
-def hindex(liste:list) -> list:
-    k = [1 for i in range(len(list))]
-    for i in range(len(list)):
-        k[i] = len([1 for j in list[:i] if j>k[i]])
-    return k
-
-hindex([5,1,2])
