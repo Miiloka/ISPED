1eb961097bd2555af37594a2b0e3cd27211abc3b
Adrien <magiczvirtuoz@gmail.com>
2023-01-28 17:28:43 +0100

    Delete Divagations avec R directory

--- Divagations avec R/calculsEPI101.R
+++
@@ -1,8 +0,0 @@
-library(epitools)
-library(epiR)
-
-tapw <- c("Lowest", "Intermediate", "Highest")
-outc <- c("Case", "Control")
-dat <- matrix(c(2, 29, 35, 64, 12, 6),3,2,byrow=TRUE)
-dimnames(dat) <- list("Tap water exposure" = tapw, "Outcome" = outc)
-oddsratio(dat, rev="c")
